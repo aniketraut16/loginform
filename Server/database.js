a8c240fc9593ad9a5371fc0ee8120f75e9f4d317
aniketraut16 <rautan_1@rknec.edu>
2024-01-31 21:25:11 +0530

    updated

--- Server/database.js
+++ Server/database.js
@@ -9,16 +9,25 @@ const dbUri = `${process.env.DB_URI}/backend`;
 
 
 async function connectToDatabase() {
-  try {
-    await mongoose.connect(databaseURI, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 3000 });
+  const maxRetries = 3;
+  let retries = 0;
 
-    console.log("Successfully connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+  while (retries < maxRetries) {
+    try {
+      await mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 10000 });
+      console.log("Successfully connected to MongoDB");
+      break;
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      retries++;
+      console.log(`Retrying connection (attempt ${retries}/${maxRetries})...`);
+      await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for 5 seconds before retrying
+    }
   }
 }
 
 connectToDatabase();
+
 
 const loginModel = new mongoose.model("loginfo", loginSchema);
 
